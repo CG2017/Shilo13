5bd49168ba7e2f5816c62213021a7bff9951ccec
Shilo Denis <shilo.denis.1997g@gmail.com>
2017-02-21 17:04:35 +0300

    hsv without suggestion colors added

--- lab1/lab1/MainWindow.xaml.cs
+++ lab1/lab1/MainWindow.xaml.cs
@@ -26,11 +26,15 @@ namespace lab1
         private readonly Point startGradientPoint = new Point(0, 0.5);
         private readonly Point endGradientPoint = new Point(1, 0.5);
 
+        private double x;
+
+
         public MainWindow()
         {
             InitializeComponent();
             SubscribeRgb();
             SubscribeCmy();
+            SubscribeHsv();
             UpdateRgbFromSliders(new object(), new EventArgs());
         }
 
@@ -118,6 +122,9 @@ namespace lab1
             double c, m, y;
             ConvertFromRgbToCmy(r, g, b, out c, out m, out y);
             UpdateCmy(c, m, y);
+            double h, s, v;
+            ConvertFromRgbToHsv(r, g, b, out h, out s, out v);
+            UpdateHsv(h, s, v);
         }
 
         private void UpdateRgb(byte r, byte g, byte b)
@@ -148,7 +155,7 @@ namespace lab1
         }
         #endregion
 
-        private Color GetColorFromCmy(double c, double m, double y)
+        private Color GetRgbColorFromCmy(double c, double m, double y)
         {
             double r = (1 - c) * 255;
             double g = (1 - m) * 255;
@@ -162,6 +169,104 @@ namespace lab1
             c = 1.0 - r/255.0;
             m = 1.0 - g/255.0;
             y = 1.0 - b/255.0;
+        }
+
+        private Color GetRgbColorFromHsv(double H, double S, double V)
+        {
+            byte R, G, B;
+            if (Math.Abs(S) < eps) //HSV from 0 to 1
+            {
+                R = (byte) (V*255);
+                G = (byte) (V*255);
+                B = (byte) (V*255);
+            }
+            else
+            {
+                double var_h = H*6;
+                if (Math.Abs(var_h - 6) < eps)
+                    var_h = 0; //H must be < 1
+                int var_i = (int) var_h; //Or ... var_i = floor( var_h )
+                double var_1 = V*(1 - S);
+                double var_2 = V*(1 - S*(var_h - var_i));
+                double var_3 = V*(1 - S*(1 - (var_h - var_i)));
+                double var_r, var_g, var_b;
+                if (var_i == 0)
+                {
+                    var_r = V;
+                    var_g = var_3;
+                    var_b = var_1;
+                }
+                else if (var_i == 1)
+                {
+                    var_r = var_2;
+                    var_g = V;
+                    var_b = var_1;
+                }
+                else if (var_i == 2)
+                {
+                    var_r = var_1;
+                    var_g = V;
+                    var_b = var_3;
+                }
+                else if (var_i == 3)
+                {
+                    var_r = var_1;
+                    var_g = var_2;
+                    var_b = V;
+                }
+                else if (var_i == 4)
+                {
+                    var_r = var_3;
+                    var_g = var_1;
+                    var_b = V;
+                }
+                else
+                {
+                    var_r = V;
+                    var_g = var_1;
+                    var_b = var_2;
+                }
+
+                R = (byte) (var_r*255); //RGB results from 0 to 255
+                G = (byte) (var_g*255);
+                B = (byte) (var_b*255);
+            }
+            return Color.FromRgb(R, G, B);
+        }
+
+        private void ConvertFromRgbToHsv(
+            byte R, byte G, byte B, out double H, out double S, out double V)
+        {
+            double var_R = (R/255.0); //RGB from 0 to 255
+            double var_G = (G/255.0);
+            double var_B = (B/255.0);
+
+            double var_Min = Math.Min(var_R, Math.Min(var_G, var_B)); //Min. value of RGB
+            double var_Max = Math.Max(var_R, Math.Max(var_G, var_B)); //Max. value of RGB
+            double del_Max = var_Max - var_Min; //Delta RGB value 
+
+            V = var_Max;
+
+            if (Math.Abs(del_Max) < eps) //This is a gray, no chroma...
+            {
+                H = 0; //HSV results from 0 to 1
+                S = 0;
+            }
+            else //Chromatic data...
+            {
+                S = del_Max/var_Max;
+
+                double del_R = (((var_Max - var_R)/6) + (del_Max/2))/del_Max;
+                double del_G = (((var_Max - var_G)/6) + (del_Max/2))/del_Max;
+                double del_B = (((var_Max - var_B)/6) + (del_Max/2))/del_Max;
+
+                if (Math.Abs(var_R - var_Max) < eps) H = del_B - del_G;
+                else if (Math.Abs(var_G - var_Max) < eps) H = (1/3.0) + del_R - del_B;
+                else H = (2/3.0) + del_G - del_R;
+
+                if (H < 0) H += 1;
+                if (H > 1) H -= 1;
+            }
         }
 
         #region CMY
@@ -217,12 +322,17 @@ namespace lab1
         private void UpdateFromCmyModel(double c, double m, double y)
         {
             UpdateCmy(c, m, y);
-            Color rgbColor = GetColorFromCmy(c, m, y);
+            Color rgbColor = GetRgbColorFromCmy(c, m, y);
             UpdateRgb(rgbColor.R, rgbColor.G, rgbColor.B);
+            double h, s, v;
+            ConvertFromRgbToHsv(rgbColor.R, rgbColor.G, rgbColor.B,
+                out h, out s, out v);
+            UpdateHsv(h, s, v);
         }
 
         private void UpdateCmy(double c, double m, double y)
         {
+            DesubscribeCmy();
             SetSliderValue(CSlider, c);
             SetSliderValue(MSlider, m);
             SetSliderValue(YSlider, y);
@@ -231,7 +341,7 @@ namespace lab1
             SetTextBoxText(MValueTextBox, m.ToString(CultureInfo.CurrentCulture));
             SetTextBoxText(YValueTextBox, y.ToString(CultureInfo.CurrentCulture));
 
-            Color rgbColor = GetColorFromCmy(c, m, y);
+            Color rgbColor = GetRgbColorFromCmy(c, m, y);
             CGradientRect.Fill = new LinearGradientBrush(
                 Color.FromRgb(255, rgbColor.G, rgbColor.B), Color.FromRgb(0, rgbColor.G, rgbColor.B),
                 startGradientPoint, endGradientPoint);
@@ -241,10 +351,59 @@ namespace lab1
             YGradientRect.Fill = new LinearGradientBrush(
                 Color.FromRgb(rgbColor.R, rgbColor.G, 255), Color.FromRgb(rgbColor.R, rgbColor.G, 0),
                 startGradientPoint, endGradientPoint);
+            SubscribeCmy();
         }
-        
+
+        #endregion
+
+#region HSV
+
+        private void SubscribeHsv()
+        {
+            HSlider.ValueChanged += UpdateHsvFromSliders;
+            SSlider.ValueChanged += UpdateHsvFromSliders;
+            VSlider.ValueChanged += UpdateHsvFromSliders;
+        }
+
+        private void DesubscribeHsv()
+        {
+            HSlider.ValueChanged -= UpdateHsvFromSliders;
+            SSlider.ValueChanged -= UpdateHsvFromSliders;
+            VSlider.ValueChanged -= UpdateHsvFromSliders;
+        }
+
+        private void UpdateHsvFromSliders(object sender, EventArgs args)
+        {
+            double h = HSlider.Value;
+            double s = SSlider.Value;
+            double v = VSlider.Value;
+            UpdateFromHsvModel(h, s, v);
+        }
+
+        private void UpdateFromHsvModel(double h, double s, double v)
+        {
+            UpdateHsv(h, s, v);
+            Color color = GetRgbColorFromHsv(h, s, v);
+            UpdateRgb(color.R, color.G, color.B);
+            double c, m, y;
+            ConvertFromRgbToCmy(color.R, color.G, color.B, out c, out m, out y);
+            UpdateCmy(c, m, y);
+        }
+
+        private void UpdateHsv(double h, double s, double v)
+        {
+            DesubscribeHsv();
+
+            SetTextBoxText(HValueTextBox, (h*365).ToString(CultureInfo.CurrentCulture));
+            SetTextBoxText(SValueTextBox, s.ToString(CultureInfo.CurrentCulture));
+            SetTextBoxText(VValueTextBox, v.ToString(CultureInfo.CurrentCulture));
+
+            SetSliderValue(HSlider, h);
+            SetSliderValue(SSlider, s);
+            SetSliderValue(VSlider, v);
+
+            SubscribeHsv();
+        }
 #endregion
-
-
     }
 }
